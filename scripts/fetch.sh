5f433ef793b3ed6d3b2ad8c998c62a6d71a625f6
Bhargava Shastry <bshastry@sec.t-labs.tu-berlin.de>
2016-04-04 14:12:13 +0200

    Updated fetch to not refetch if folder already exists

--- scripts/fetch.sh
+++ scripts/fetch.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+if [ ! -e joern-0.3.1 ]; then
 echo -e "\t[+] Installing Joern"
 echo -e "\t\t[+] Installing Joern core"
 wget https://github.com/fabsx00/joern/archive/0.3.1.tar.gz &> /dev/null
@@ -8,7 +9,9 @@ tar xfz lib.tar.gz && rm lib.tar.gz
 ant &> /dev/null && ant tools &> /dev/null
 echo "alias joern='java -jar `pwd`/bin/joern.jar'" > ~/.bash_aliases
 source ~/.bashrc
+fi
 
+if [ ! -e neo4j-community-2.1.8 ]; then
 echo -e "\t\t[+] Installing Neo4J 2.1.8"
 cd .. && wget http://neo4j.com/artifact.php?name=neo4j-community-2.1.8-unix.tar.gz -O neo4j-community-2.1.8-unix.tar.gz &> /dev/null
 tar xzf neo4j-community-2.1.8-unix.tar.gz && rm neo4j-community-2.1.8-unix.tar.gz
@@ -19,13 +22,16 @@ echo -e "\t\t[+] Installing prebuilt Gremlin"
 wget http://mlsec.org/joern/lib/neo4j-gremlin-plugin-2.1-SNAPSHOT-server-plugin.zip &> /dev/null
 unzip neo4j-gremlin-plugin-2.1-SNAPSHOT-server-plugin.zip -d $Neo4jDir/plugins/gremlin-plugin &> /dev/null
 rm neo4j-gremlin-plugin-2.1-SNAPSHOT-server-plugin.zip
+fi
 
+if [ ! -e python-joern-0.3.1 ]; then
 echo -e "\t\t[+] Install python-joern"
 sudo pip install py2neo &> /dev/null
 wget https://github.com/fabsx00/python-joern/archive/0.3.1.tar.gz &> /dev/null
 tar xfz 0.3.1.tar.gz && rm 0.3.1.tar.gz && cd python-joern-0.3.1
 sudo python2 setup.py install &> /dev/null
 cd ..
+fi
 
 echo -e "\t\t[+] Adding SecT gitlab public key to known hosts"
 HOST="gitlab.sec.t-labs.tu-berlin.de"
@@ -33,16 +39,21 @@ touch ~/.ssh/known_hosts
 ssh-keyscan -t rsa,dsa $HOST 2>&1 | sort -u - ~/.ssh/known_hosts > ~/.ssh/tmp_hosts
 cat ~/.ssh/tmp_hosts >> ~/.ssh/known_hosts
 
+if [ ! -e joern-tools ]; then
 echo -e "\t\t[+] Installing joern tools"
 git clone git@gitlab.sec.t-labs.tu-berlin.de:mleutner/joern-tools.git &> /dev/null
 cd joern-tools && sudo python2 setup.py install &> /dev/null
 cd ..
+fi
 
+if [ ! -e afl* ]; then
 echo -e "\t[+] Installing afl-fuzz latest"
 wget http://lcamtuf.coredump.cx/afl/releases/afl-latest.tgz &> /dev/null
 tar xzf afl-latest.tgz && rm afl-latest.tgz && cd afl*
 make -j &> /dev/null && sudo make install &> /dev/null && cd ..
+fi
 
+if [ ! -e exploitable ]; then
 echo -e "\t[+] Setting up gdb"
 echo -e "\t\t[+] Install gdb exploitable plugin"
 git clone https://github.com/jfoote/exploitable.git &> /dev/null
@@ -63,7 +74,9 @@ define printfault
     printf "Faulting mem location is %#lx, pc is %#lx, rsp is %#lx, rbp is %#lx\n", \$_siginfo._sifields._sigfault.si_addr, \$pc, \$rsp, \$rbp
 end
 EOF
+fi
 
+if [ ! -e workspace ]; then
 echo -e "\t[+] Creating workspace and fetching a couple of production codebases"
 mkdir -p workspace && cd workspace
 echo -e "\t\t[+] Fetching (buggy) OpenvSwitch 2.3.2"
@@ -96,3 +109,4 @@ done
 EOF
 chmod +x getcrashlog.sh
 ./getcrashlog.sh &>> crash.log
+fi
